0739d01b4bfa570c771d11e30e67badcf64eff8d
claw <themandibleclaw@gmail.com>
2026-01-15 23:20:47 +0000

    fix code review comments

--- app/main.py
+++ app/main.py
@@ -21,17 +21,11 @@ class Distance:
             self.km += other
         return self
 
-    def __mul__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(self.km * other.km)
-        else:
-            return Distance(self.km * other)
+    def __mul__(self, other: int | float) -> Distance:
+        return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance | int | float) -> Distance:
-        if isinstance(other, Distance):
-            return Distance(round(self.km / other.km, 2))
-        else:
-            return Distance(round(self.km / other, 2))
+    def __truediv__(self, other: int | float) -> Distance:
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
