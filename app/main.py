36290b83b2f9cc8b01ca12647add134d5619ef15
claw <themandibleclaw@gmail.com>
2026-01-15 23:12:59 +0000

    fix code review comments

--- app/main.py
+++ app/main.py
@@ -20,43 +20,43 @@ class Distance:
         else:
             self.km += other
 
-    def __mul__(self, other) -> Distance:
+    def __mul__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km * other.km)
         else:
             return Distance(self.km * other)
 
-    def __truediv__(self, other) -> Distance:
+    def __truediv__(self, other: Distance | int) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km / other.km)
         else:
             return Distance(self.km / other)
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         else:
             return self.km < other
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         else:
             return self.km > other
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         else:
             return self.km <= other
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         else:
             return self.km >= other
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: Distance | int) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         else:
