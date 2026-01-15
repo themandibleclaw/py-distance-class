7ff7b420463c5f47486f5dc2591b85aa716ffe1a
claw <themandibleclaw@gmail.com>
2026-01-15 23:30:37 +0000

    fix code review comments

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
