ed1964d02d5bfaa08daf1deae3ba0fffe3a8d7dc
claw <themandibleclaw@gmail.com>
2026-01-15 22:54:16 +0000

    implement requirements

--- app/main.py
+++ app/main.py
@@ -1,3 +1,33 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers"
+
+    def __add__(self, other) -> Distance:
+        return Distance(self.km + other.km)
+
+    def __iadd__(self, other):
+        self.km += other.km
+
+    def __mul__(self, other) -> Distance:
+        return Distance(self.km * other.km)
+
+    def __truediv__(self, other):
+        return Distance(self.km / other.km)
+
+    def __lt__(self, other):
+        return self.km < other.km
+
+    def __gt__(self, other):
+        return self.km > other.km
+
+    def __le__(self, other):
+        return self.km <= other.km
+
+    def __ge__(self, other):
+        return self.km >= other.km
+
+    def __eq__(self, other):
+        return self.km == other.km
